1fbc590cb6a5900f7b2d6bc1e8e6efcf2186f76f
arhab07 <arhab07ah@gmail.com>
2022-03-04 14:26:34 +0530

    Dice app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet var diceImage1: UIImageView!
+    
+    @IBOutlet var diceImage2: UIImageView!
+//    var leftButton = Int.random(in: 0...5)
+//    var rightButton = Int.random(in: 0...5)
+    override func viewDidLoad() {//block of code starts here!!
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        diceImage1.image = #imageLiteral(resourceName: "DiceThree") //who.what= value (dot notation)
+//        diceImage1.alpha = 1
+//        diceImage2.image = #imageLiteral(resourceName: "DiceFive")
+//
     }
 
-
+    @IBAction func rollDiceButton(_ sender: UIButton) {
+        let rightButton = Int.random(in: 0...5)
+       let leftButton = Int.random(in: 0...5)
+        diceImage1.image = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftButton]
+print("lefthand dice is \(leftButton)")
+        //        leftButton += 1
+        
+        diceImage2.image = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightButton]
+//        rightButton -= 1
+        print("Right hand dice is \(rightButton)")
+            }
+    
 }
 
